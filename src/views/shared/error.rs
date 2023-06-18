7493a23f2f9b8d9900304f213ece44c80e6a1a43
Viveret Steele <viveret.amant@gmail.com>
2023-06-17 19:56:06 -0700

    Finish error view

--- src/views/shared/error.rs
+++ src/views/shared/error.rs
@@ -1,12 +1,11 @@
 mvc_macro_lib::rusthtml_view_macro! {
     @viewstart "src/views/dev/_view_start.rshtml"
-    @use mvc_lib::error::error_viewmodel_service::BasicErrorViewModel
     @name "error"
-    @model BasicErrorViewModel
+    @model mvc_lib::error::error_viewmodel_service::BasicErrorViewModel
     @{
         view_context.insert_str("Title", "Error".to_string());
     }
-    <div>
-    @format!("Error: {}", model.error)
-    </div>
+    <h1>@view_context.get_str("Title")</h1>
+    <h3>@format!("{}", model.error)</h3>
+    <p>@format!("Source: {:?}", model.error.source())</p>
 }

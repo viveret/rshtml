3d5326e43b7efda001c88199df1c4c6e830fd952
Viveret Steele <viveret.amant@gmail.com>
2023-12-07 06:08:32 -0800

    idk these changes

--- core_lib/src/assert/assert_tokentree.rs
+++ core_lib/src/assert/assert_tokentree.rs
@@ -1,38 +1,42 @@
 
 
-use proc_macro2::TokenTree;
+use proc_macro2::{TokenTree, Literal, Ident, Group, Punct};
 
 // assert that the token is a punct with the given value
-pub fn assert_tokentree_punct(punct: &TokenTree, expected: char) {
+pub fn assert_tokentree_punct(punct: &TokenTree, expected: char) -> Punct {
     if let TokenTree::Punct(punct) = punct {
         assert_eq!(punct.as_char(), expected);
+        punct.clone()
     } else {
         panic!("expected punct, received {:?}", punct);
     }
 }
 
 // assert that the token is a group with the given delimiter
-pub fn assert_tokentree_group(group: &TokenTree, expected: proc_macro2::Delimiter) {
+pub fn assert_tokentree_group(group: &TokenTree, expected: proc_macro2::Delimiter) -> Group {
     if let TokenTree::Group(group) = group {
         assert_eq!(group.delimiter(), expected);
+        group.clone()
     } else {
         panic!("expected group, received {:?}", group);
     }
 }
 
 // assert that the token is an ident with the given value
-pub fn assert_tokentree_ident(ident: &TokenTree, expected: &str) {
+pub fn assert_tokentree_ident(ident: &TokenTree, expected: &str) -> Ident {
     if let TokenTree::Ident(ident) = ident {
         assert_eq!(ident.to_string(), expected);
+        ident.clone()
     } else {
         panic!("expected ident, received {:?}", ident);
     }
 }
 
 // assert that the token is a literal with the given value
-pub fn assert_tokentree_literal(literal: &TokenTree, expected: &str) {
+pub fn assert_tokentree_literal(literal: &TokenTree, expected: &str) -> Literal {
     if let TokenTree::Literal(literal) = literal {
         assert_eq!(literal.to_string(), expected);
+        literal.clone()
     } else {
         panic!("expected literal, received {:?}", literal);
     }
